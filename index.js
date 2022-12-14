95d1cb7536f25367c2cfb976bd237ca1294c942c
Gabriella Elizondo <gabriellaelizondo@gmail.com>
2022-12-14 18:21:51 -0500

    commit tree c4e689e33f39d32176cd67666284803c03200db0

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const employee = {
+  firstName: "Gabriella Elizondo",
+  streetAddress: "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newObj = { ...employee };
+  newObj.firstName = "Sam";
+  streetAddress = "11 Broadway";
+  return newObj;
+}
